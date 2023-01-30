39cacd865af6521117582d1cf124d32bc6d57817
Yury Benesh <yury.benesh@yandex.by>
2023-01-30 09:33:38 +0200

    improved find_dup.d

--- find_dup/find_dup.d
+++ find_dup/find_dup.d
@@ -2,6 +2,7 @@ import std.stdio;
 import std.file;
 import std.path;
 import std.sumtype;
+import std.regex;
 
 alias FileOrMulti = SumType!(string, string[]);
 alias PathsMap = FileOrMulti[string];
@@ -35,6 +36,29 @@ void print_paths(in FileOrMulti files) {
 }
 
 void process_dups(in PathsMap listA, in PathsMap listB) {
+// print file name, then the paths it's found in listA,
+// then in listB
+// then print the same for listB
+    foreach(k, v; listA) {
+        bool printed_paths = false;
+        v.match!(
+            (in string[] m) {
+                writeln(k);
+                print_paths(v);
+                printed_paths = true;
+            },
+            (_) {},
+        );
+        if (k in listB) {
+            if (!printed_paths) {
+                writeln(k);
+                print_paths(v);
+            }
+            print_paths(listB[k]);
+        }
+    }
+
+/*
     foreach (k, v; listA)
     {
         if (k in listB) {
@@ -42,24 +66,27 @@ void process_dups(in PathsMap listA, in PathsMap listB) {
             print_paths(listB[k]);
         }
     }
+    */
 }
 
 void usage() {
-    writeln("Usage: find_dup <path a> <path b>");
+    writeln("Usage: find_dup <path a> [path b]");
 }
 
 int main(string[] args) {
-    if (args.length != 3) {
+    if (args.length < 2 && args.length > 3) {
         usage();
         return 1;
     }
 
     const pathA = args[1];
-    const pathB = args[2];
+    const pathB = args.length == 3 ? args[2] : null;
 
-    if (pathA.exists() && pathB.exists() && pathA.isDir() && pathB.isDir()) {
+    const pathBok = !pathB || (pathB.exists() && pathB.isDir());
+
+    if (pathA.exists() && pathA.isDir() && pathBok ) {
         const listA = gather_files(pathA);
-        const listB = gather_files(pathB);
+        const listB = pathB ? gather_files(pathB) : new PathsMap;
         process_dups(listA, listB);
         return 0;
     }
