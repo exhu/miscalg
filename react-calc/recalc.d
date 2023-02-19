74780df1122b02eaad556773a3e2a59ebfd794ac
Yury Benesh <yury.benesh@yandex.by>
2023-02-19 16:58:01 +0200

    recalc.d

---
+++ react-calc/recalc.d
@@ -0,0 +1,154 @@
+module recalc;
+import std.stdio : writefln, writeln;
+import std.format : format;
+
+// build dependency tree
+// on changes mark nodes to be updated in dep. order
+// for each marked node do recalc
+
+/*
+a
+b = a + 3
+c = b * d
+d = a + 4
+e = c + d
+
+a -> b
+a -> d
+b -> c
+d -> c
+c -> e
+d -> e
+
+
+if 'a' changes:
+    update b (aN, 3), c (bN, dOLD!)
+
+*/
+
+class ValueNode
+{
+    void update(int newData)
+    {
+        writefln("update %s with %d, old %d", name, newData, data);
+        data = newData;
+    }
+
+    void updateWithDeps(int newData)
+    {
+        update(newData);
+        foreach (i; deps)
+        {
+            i.updateWithDeps(data);
+        }
+    }
+
+    this(immutable string aName)
+    {
+        name = aName;
+    }
+
+    override string toString() const @safe 
+    {
+        string sdeps;
+        foreach (d; deps)
+        {
+            sdeps ~= format("%s,\n",d.toString());
+        }
+        string result = format("%s=%d,[%s]", name, data, sdeps);
+        return result;
+    }
+
+    int data = 0;
+    immutable string name;
+    ValueNode[] deps;
+}
+
+ValueNode build()
+{
+    ValueNode a = new ValueNode("a");
+    auto b = new ValueNode("b");
+    auto c = new ValueNode("c");
+    auto d = new ValueNode("d");
+    auto e = new ValueNode("e");
+
+    /*
+    a -> b
+    a -> d
+    b -> c
+    d -> c
+    c -> e
+    d -> e
+    */
+    a.deps ~= b;
+    a.deps ~= d;
+    b.deps ~= c;
+    d.deps ~= c;
+    c.deps ~= e;
+    d.deps ~= e;
+
+    return a;
+}
+
+ValueNode[] topSorted(ValueNode start)
+{
+    bool[ValueNode] nodes = [start: true];
+    struct Edge
+    {
+        ValueNode from, to;
+        string toString() const
+        {
+            return "from " ~ from.name ~ " to " ~ to.name;
+        }
+    }
+    Edge[] edges;
+    // discover all nodes and edges
+    void collect(ValueNode s)
+    {
+        foreach (n; s.deps)
+        {
+            nodes[n] = true;
+            edges ~= Edge(s, n);
+            collect(n);
+        }
+    }
+
+    collect(start);
+
+    import std.algorithm : map;
+    import std.conv : to;
+    writefln("Nodes = %s", nodes.keys.map!(n => n.name));
+    writefln("Edges = %s", edges.map!(to!string));
+
+    ValueNode[] result;
+    return result;
+}
+
+void updateTopsorted()
+{
+    writeln("updateTopSorted");
+    auto root = build();
+
+    topSorted(root);
+    /*
+    writefln("before = %s", root);
+    // TODO build topologically sorted graph
+    writefln("after = %s", root);
+    */
+
+}
+
+void updateWithDeps()
+{
+    writeln("updateWithDeps");
+    auto root = build();
+    writefln("before = %s", root);
+    root.updateWithDeps(1);
+    writefln("after = %s", root);
+}
+
+void main()
+{
+    updateWithDeps();
+    updateTopsorted();
+}
