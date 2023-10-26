dac6b88969c67112e1995eb92f0b492be8664103
Yury Benesh <yury.benesh@yandex.by>
2023-10-26 09:33:52 +0300

    calc-r

--- calc-r/src/main.rs
+++ calc-r/src/main.rs
@@ -37,6 +37,13 @@ fn is_number(c: char) -> bool {
 
 fn read_number(src: &[char], start: usize) -> Token {
     let mut chars_read = 0;
+    for i in start..src.len() {
+        let c = src[i];
+        if !is_number(c) {
+            break;
+        }
+        chars_read += 1;
+    }
 
     let back_str: String = src[start..start + chars_read].iter().collect();
     let num = back_str.parse().unwrap();
@@ -44,12 +51,22 @@ fn read_number(src: &[char], start: usize) -> Token {
         kind: TokenKind::Number { value: num },
         span: TokenSpan {
             start: start,
-            end: chars_read,
+            end: start + chars_read - 1,
         },
     }
 }
 
+fn skip_whitespace(src: &[char], start: usize) -> usize {
+    for i in start..src.len() {
+        if !src[i].is_ascii_whitespace() {
+            return i;
+        }
+    }
+    return src.len() - 1;
+}
+
 fn read_token(src: &[char], start: usize) -> Token {
+    let start = skip_whitespace(src, start);
     let c = src[start];
     if is_number(c) {
         read_number(src, start)
