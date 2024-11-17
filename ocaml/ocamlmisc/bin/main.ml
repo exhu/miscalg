a04709e169663133c63c0da67df29a8a0547205d
Yury Benesh <yury.benesh@yandex.by>
2024-11-17 13:41:37 +0200

    ocaml: added formatting

--- ocaml/ocamlmisc/bin/main.ml
+++ ocaml/ocamlmisc/bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+open Ocamlmisc.Tsorta
+
+let () =
+  print_endline "hi";
+  let a : Graph.edge = { from_cell = 0; to_cell = 1 } in
+  let b : Graph.edge = { from_cell = 2; to_cell = 1 } in
+  let edges = [| a; b |] in
+  let g = Graph.from_edges edges in
+  Graph.dump g
--- ocaml/ocamlmisc/lib/tsorta.ml
+++ ocaml/ocamlmisc/lib/tsorta.ml
@@ -1,51 +1,72 @@
-
-let () = print_endline "hello";
+open Base
 
 (* https://en.wikipedia.org/wiki/Topological_sorting *)
-(* "...for every directed edge (u,v) from vertex u to vertex v, u comes before v in the ordering." *)
+(* "...for every directed edge (u,v) from vertex u to vertex v, u comes before
+   v in the ordering." *)
 
 (* Dependency application: u is the source, v is the product/dependent on u. *)
 
 (* TODO cells must be indexed, array or map? *)
 type cells = string list
 
-
-
 module Graph = struct
-        type edge = { from_cell: int;
-                to_cell: int }
+  type edge = { from_cell : int; to_cell : int }
+  type edges = edge array
+  type t = { edges : edges; nodes_count : int }
 
-        type edges = edge array
-        type t = { edges: edges ; nodes_count: int }
+  let nodes_count t = t.nodes_count
+  let from_cell (e : edge) = e.from_cell
 
-let from_edges (e: edges) =
-        let nodes_count = Array.fold_left (fun a c -> max a (max c.from_cell c.to_cell)) 0 e in
-                { edges = e ; nodes_count }
+  let from_edges (e : edges) =
+    let nodes_count =
+      Array.fold ~f:(fun a c -> max a (max c.from_cell c.to_cell)) ~init:0 e
+    in
+    { edges = e; nodes_count }
 
-
-let dump (t: t): unit = for i = 0 to (Array.length t.edges-1) do
-        let e = t.edges.(i) in
-               Printf.printf "edge %d -> %d\n" e.from_cell e.to_cell
-        done
+  let dump (t : t) : unit =
+    for i = 0 to Array.length t.edges - 1 do
+      let e = t.edges.(i) in
+      Stdio.printf "edge %d -> %d\n" e.from_cell e.to_cell
+    done
 end
 
+(* depth first
+   L ← Empty list that will contain the sorted nodes
+   while exists nodes without a permanent mark
+   do select an unmarked node n
+   visit(n)
 
-(* TODO implement different algorigthms *)
+   function visit(node n)
+   if n has a permanent mark then return
+   if n has a
+   temporary mark then stop   (graph has at least one cycle)
 
-(*
-let tsortdf e: edges =
-        let all_indexes = List.fold_left (fun a c -> (a :: [c.from_cell ; c.to_cell])) [] in
-        ;
+       mark n with a temporary mark
+
+       for each node m with an edge from n to m do visit(m)
+
+       mark n with a permanent mark add n to head of L
 *)
 
-let () = print_endline "hi" ;
-        let a: Graph.edge = {
-                from_cell = 0 ;
-                to_cell = 1
-        }
-        in
-        let b: Graph.edge  = { from_cell = 2 ; to_cell = 1 }  in
-        let edges = [| a ; b |] in
-        let g = Graph.from_edges edges in
-        Graph.dump g
- 
+let sorted_l = []
+let perm_marked = []
+let temp_marked = []
+
+let source_nodes (e : Graph.edges) =
+  Array.map ~f:Graph.from_cell e |> List.of_array
+
+let visit n perm_marked temp_marked sorted_l =
+  match List.find perm_marked ~f:(( = ) n) with
+  | Some _ -> Some sorted_l
+  | None -> None (*todo*)
+
+let sorted_or_none = []
+
+(* let depth_first a:Graph.t = let unmarked_nodes = List.range 0
+   (Graph.nodes_count a) in unmarked_nodes
+*)
+(* TODO implement different algorigthms *)
+
+(* let tsortdf e: edges = let all_indexes = List.fold_left (fun a c -> (a ::
+   [c.from_cell ; c.to_cell])) [] in ;
+*)
