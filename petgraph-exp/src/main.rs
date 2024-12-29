b08b8b4f1335b92e041656a9ab69b096c667e049
Yury Benesh <yury.benesh@yandex.by>
2024-12-29 21:46:34 +0200

    context wip

--- petgraph-exp/src/main.rs
+++ petgraph-exp/src/main.rs
@@ -1,7 +1,43 @@
+use std::ops::Index;
+
 use petgraph::algo::toposort;
 use petgraph::graph::DiGraph;
+use petgraph::visit::GraphBase;
 
 type MyGraph = DiGraph<&'static str, ()>;
+type NodeId = <MyGraph as GraphBase>::NodeId;
+
+#[derive(Debug)]
+struct Context {
+    graph: MyGraph,
+    toposorted: Vec<NodeId>,
+}
+
+impl Context {
+    pub fn new() -> Context {
+        let g = make_graph();
+
+        let sorted = toposort(&g, None);
+        let sorted = match sorted {
+            Ok(nodes) => {
+                println!("Sorted = {:?}", nodes);
+                nodes
+            }
+            Err(cycle) => {
+                println!("Cycle = {:?}", cycle);
+                Vec::new()
+            }
+        };
+        Context {
+            graph: g,
+            toposorted: sorted,
+        }
+    }
+
+    pub fn invalidate_node(&self, name: &str) {
+        // construct a list of affected nodes preserving topological sorting
+    }
+}
 
 fn make_graph() -> MyGraph {
     let mut g = MyGraph::new();
@@ -11,6 +47,7 @@ fn make_graph() -> MyGraph {
     let d = g.add_node("d");
     let e = g.add_node("e");
     g.add_edge(a, b, ());
+    g.add_edge(a, c, ());
     g.add_edge(b, c, ());
     g.add_edge(d, e, ());
 
@@ -18,12 +55,8 @@ fn make_graph() -> MyGraph {
 }
 
 fn main() {
-    let g = make_graph();
-    let sorted = toposort(&g, None);
-    match sorted {
-        Ok(nodes) => println!("Sorted = {:?}", nodes),
-        Err(cycle) => println!("Cycle = {:?}", cycle),
-    }
+    let c = Context::new();
+    c.invalidate_node("a");
 
-    println!("Hello, world! {:?}", g);
+    println!("Hello, world! {:?}", c);
 }
