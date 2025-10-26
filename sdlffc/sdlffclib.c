15aec7815ee2242bfc453a2c7964ad9a606a38ff
Yury Benesh <yurybdev@gmail.com>
2025-10-26 12:30:39 +0200

    create streaming texture

--- sdlffc/sdlffclib.c
+++ sdlffc/sdlffclib.c
@@ -3,6 +3,7 @@
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_hints.h>
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 #include <memory.h>
@@ -11,9 +12,10 @@ bool sdlffclib_init(SdlffContext **out_context) {
   static SdlffContext global_context = {
       .window = NULL,
       .renderer = NULL,
+      .streaming_texture = NULL,
   };
 
-  SDL_SetAppMetadata("sdlffc", "0.1", "com.github.exhu.miscalg.sdlffc");
+  SDL_SetAppMetadata("rdlffc", "0.1", "com.github.exhu.miscalg.sdlffc");
 
   if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to init: %s",
@@ -33,12 +35,23 @@ bool sdlffclib_init(SdlffContext **out_context) {
   }
   SDL_SetWindowMinimumSize(context->window, 320, 200);
   SDL_SetRenderVSync(context->renderer, SDL_RENDERER_VSYNC_ADAPTIVE);
-  // TODO create streaming texture for video
+
+  // TODO move create streaming texture for video somewhere else
+  context->streaming_texture =
+      SDL_CreateTexture(context->renderer, SDL_PIXELFORMAT_YV12,
+                        SDL_TEXTUREACCESS_STREAMING, 320, 240);
+
+  if (context->streaming_texture == NULL) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to create texture: %s",
+                 SDL_GetError());
+  }
+
   // TODO https://github.com/libsdl-org/SDL/blob/main/test/testffmpeg.c
   return true;
 }
 
 void sdlffclib_done(SdlffContext **out_context) {
+  SDL_DestroyTexture((*out_context)->streaming_texture);
   SDL_DestroyRenderer((*out_context)->renderer);
   SDL_DestroyWindow((*out_context)->window);
   memset(*out_context, 0, sizeof(SdlffContext));
@@ -63,4 +76,5 @@ void sdlffclib_main_loop(SdlffContext *context) {
     if (event.type == SDL_EVENT_WINDOW_EXPOSED)
       sdlffclib_render(context);
   }
+  SDL_Log("Quit.");
 }
--- sdlffc/sdlffclib_private.h
+++ sdlffc/sdlffclib_private.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_render.h>
 
 struct _SdlffContext {
   SDL_Window *window;
   SDL_Renderer *renderer;
+  SDL_Texture *streaming_texture;
 };
