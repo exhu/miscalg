c2129283388016f5e89e96a1afba63545f2f00e2
Yury Benesh <yurybdev@gmail.com>
2026-02-27 22:19:07 +0200

    graph wip

--- dlang/dep-model/source/graph.d
+++ dlang/dep-model/source/graph.d
@@ -1,5 +1,6 @@
 import std.algorithm.iteration;
 import std.algorithm.comparison;
+import std.algorithm.searching;
 import std.stdio : writefln;
 import std.format;
 import std.traits;
@@ -37,14 +38,15 @@ struct Graph
         return filter!(f)(edges).map!m();
     }
 
-  string generate_dot_text(string name) {
-    string buf;
-    buf ~= format("digraph \"%s\" {\n", name);
-    auto e = (const Edge e) => buf ~= format("%d -> %d\n", e.from_cell, e.to_cell);
-    each!e(edges);
-    buf ~= "}";
-    return buf;
-  }
+    string generate_dot_text(string name)
+    {
+        string buf;
+        buf ~= format("digraph \"%s\" {\n", name);
+        auto e = (const Edge e) => buf ~= format("%d -> %d\n", e.from_cell, e.to_cell);
+        each!e(edges);
+        buf ~= "}";
+        return buf;
+    }
 }
 
 void print_edges(const Edges edges)
@@ -53,90 +55,166 @@ void print_edges(const Edges edges)
     each!(f)(edges);
 }
 
-struct SortResult {
-  enum Selector {
-    sorted,
-    cycle,
-  }
-  Selector result;
-  union {
-    Edges sorted;
-    CellIndex cycle;
-  }
-}
-
-private string generateMembers(E, Types...)() {
-    string members;
-    assert(Types.length >= 1);
-    static foreach(i,t ; Types){
-      members ~= "private " ~ t.stringof ~ " " ~ __traits(identifier, EnumMembers!E[i]) ~ ";";
+struct SortResult
+{
+    enum Selector
+    {
+        sorted,
+        cycle,
     }
-    return members;
-}
 
-private string generateAccessors(E, Types...)() {
-    string members;
-    assert(Types.length >= 1);
-    foreach(i,t ; Types){
-      auto eId = __traits(identifier, EnumMembers!E[i]);
-      auto eName = __traits(identifier, E);
-      auto funcName = "get_" ~ eId;
-      members ~= t.stringof ~ " " ~ funcName ~ "(){" ~
-	"assert(selector == " ~ eName ~ "." ~ eId ~ ", \"" ~ funcName ~ "\");" ~
-	"return "  ~ eId ~ ";}";
+    Selector result;
+    union
+    {
+        Edges sorted;
+        CellIndex cycle;
     }
-    return members;
-  }
-
-template adtUnion(E, Types...) if (EnumMembers!E.length == Types.length) {
-  enum string adtUnion = "private " ~ E.stringof ~ " selector; union{" ~
-    generateMembers!(E, Types)() ~ "}" ~
-    E.stringof ~ " getTag() { return selector; }" ~
-    generateAccessors!(E, Types)();
 }
 
-template adtMembers(E, Types...) if (EnumMembers!E.length == Types.length) {
-  enum string adtMembers = E.stringof ~ " selector; " ~ generateMembers!(E, Types)();
-}
+private struct SortContext
+{
+    Graph graph;
+    CellIndex[] sorted, perm_marked, temp_marked;
+    //size_t perm_marked_count;
 
-struct TestAdt {
-  enum Selector {
-    first, second, empty, fourth, fifth,
+    struct VisitStatus
+    {
+        enum Selector
+        {
+            continueVisiting,
+            cycleFound,
+        }
+
+      Selector selector;
+        CellIndex cycle;
+    }
+
+  void mark_perm(CellIndex n)
+  {
+    perm_marked ~= n;
   }
-  struct First {
-    int a = 1;
+
+  void add_to_sorted(CellIndex n)
+  {
+    sorted ~= n;
   }
-  struct Second {
-    int b = 2;
+
+  void mark_temp(CellIndex n)
+  {
+    temp_marked ~= n;
   }
-  struct Empty {}
-  struct Fourth {
-    string s = "fourth";
+
+  VisitStatus visit(CellIndex n)
+  {
+    if (!find(perm_marked, n).empty) { return VisitStatus(VisitStatus.Selector.continueVisiting); } else if (!find(temp_marked, n).empty) {
+      return VisitStatus(VisitStatus.Selector.cycleFound, n);}
+    mark_temp(n);
+    auto nodes_of_n = graph.all_dest_from(n);
+    auto is_cycle = (CellIndex c) => visit(c).selector == VisitStatus.Selector.cycleFound;
+
+    // TODO map and find
+    //    auto found = find!(is_cycle)(nodes_of_n);
+    
+
   }
-  mixin(adtUnion!(Selector, First, Second, Empty, Fourth, Empty));
-  //mixin(adtMembers!(Selector, First, Second, Empty, Fourth, Empty));
 }
 
 // TODO sort
 
-unittest
+// metaprogramming experiments
+version (none)
 {
-    import std.conv;
+    private string generateMembers(E, Types...)()
+    {
+        string members;
+        assert(Types.length >= 1);
+        static foreach (i, t; Types)
+        {
+            members ~= "private " ~ t.stringof ~ " " ~ __traits(identifier, EnumMembers!E[i]) ~ ";";
+        }
+        return members;
+    }
 
-    //writefln("%s", to!string(typeof(l)));
-    auto g = Graph.make_from_edges([Edge(0, 1), Edge(1, 2), Edge(3, 4)]);
-    assert(g.nodesCount == 5);
+    private string generateAccessors(E, Types...)()
+    {
+        string members;
+        assert(Types.length >= 1);
+        foreach (i, t; Types)
+        {
+            auto eId = __traits(identifier, EnumMembers!E[i]);
+            auto eName = __traits(identifier, E);
+            auto funcName = "get_" ~ eId;
+            members ~= t.stringof ~ " " ~ funcName ~ "(){" ~ "assert(selector == "
+                ~ eName ~ "." ~ eId ~ ", \"" ~ funcName ~ "\");" ~ "return " ~ eId ~ ";}";
+        }
+        return members;
+    }
 
+    template adtUnion(E, Types...) if (EnumMembers!E.length == Types.length)
+    {
+        enum string adtUnion = "private " ~ E.stringof ~ " selector; union{" ~ generateMembers!(E,
+                    Types)() ~ "}" ~ E.stringof ~ " getTag() { return selector; }" ~ generateAccessors!(E,
+                    Types)();
+    }
 
-    TestAdt a, b;
-    assert(a.selector == TestAdt.Selector.first);
-    b = a;
-    assert(a.first.a == 1);
-    a.fourth.s = "hh test";
-    writefln("first = %d, second = %d, third = %s", a.first.a, a.second.b, a.fourth.s);
-    string members = generateMembers!(TestAdt.Selector, TestAdt.First, TestAdt.Second);
-    writefln("%s", members);
-    a.getTag();
-    a.get_first();
-    a.get_second();
-}
+    template adtMembers(E, Types...) if (EnumMembers!E.length == Types.length)
+    {
+        enum string adtMembers = E.stringof ~ " selector; " ~ generateMembers!(E, Types)();
+    }
+
+    struct TestAdt
+    {
+        enum Selector
+        {
+            first,
+            second,
+            empty,
+            fourth,
+            fifth,
+        }
+
+        struct First
+        {
+            int a = 1;
+        }
+
+        struct Second
+        {
+            int b = 2;
+        }
+
+        struct Empty
+        {
+        }
+
+        struct Fourth
+        {
+            string s = "fourth";
+        }
+
+        mixin(adtUnion!(Selector, First, Second, Empty, Fourth, Empty));
+        //mixin(adtMembers!(Selector, First, Second, Empty, Fourth, Empty));
+    }
+
+    unittest
+    {
+        import std.conv;
+
+        //writefln("%s", to!string(typeof(l)));
+        auto g = Graph.make_from_edges([Edge(0, 1), Edge(1, 2), Edge(3, 4)]);
+        assert(g.nodesCount == 5);
+
+        TestAdt a, b;
+        assert(a.selector == TestAdt.Selector.first);
+        b = a;
+        assert(a.first.a == 1);
+        a.fourth.s = "hh test";
+        writefln("first = %d, second = %d, third = %s", a.first.a, a.second.b, a.fourth.s);
+        string members = generateMembers!(TestAdt.Selector, TestAdt.First, TestAdt.Second);
+        writefln("%s", members);
+        a.getTag();
+        a.get_first();
+        a.get_second();
+    }
+
+} // version
