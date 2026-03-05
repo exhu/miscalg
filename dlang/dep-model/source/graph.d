5b2f3834b12ec97c58c2c54cd511127fb54256c5
Yury Benesh <yurybdev@gmail.com>
2026-03-05 08:57:22 +0200

    cleanup, docs

--- dlang/dep-model/source/graph.d
+++ dlang/dep-model/source/graph.d
@@ -1,4 +1,3 @@
-// TODO clean up code
 // TODO try to use functional style -- non-member or static functions instead of methods
 // if only one field is used to simplify unit tests
 // TODO add impl using SumType
@@ -11,36 +10,55 @@ import std.string;
 
 alias CellIndex = size_t;
 
+/// Graph item.
 struct Edge
 {
+    /// Edge direction is fromCell -> toCell.
     CellIndex fromCell, toCell;
 }
 
 alias Edges = Edge[];
 
+version(unittest)
+{
+    private alias debugPrintln = writeln;
+    private alias debugPrintfln = writefln;
+}
+else
+{
+    private void debugPrintln(A...)(A _) {}
+    alias debugPrintfln = debugPrintln;
+}
+
+/// Directional graph where nodes a zero-based integers.
 struct Graph
 {
+private:
     immutable Edges edges;
     immutable size_t nodesCount;
-
+public:
+    /// The way to create a Graph.
     static Graph makeFromEdges(immutable Edges edges)
     {
         immutable maxNode = fold!((a, c) => max(a, max(c.fromCell, c.toCell)))(edges, 0L);
         return Graph(edges, maxNode + 1);
     }
 
-    void dump()
+    /// Print to stdout.
+    void dump() 
     {
         printEdges(edges);
     }
 
+    /// Get all destination nodes from n.
     auto allDestFrom(CellIndex n)
     {
-        auto f = (const Edge e) => e.fromCell == n;
-        auto m = (const Edge e) => e.toCell;
+        const f = (const Edge e) => e.fromCell == n;
+        const m = (const Edge e) => e.toCell;
         return filter!(f)(edges).map!m();
     }
 
+    /// Generate text for .gv file to be processed by dot program.
     string generateDotText(string name)
     {
         string buf;
@@ -51,64 +69,72 @@ struct Graph
         return buf;
     }
 
+    /// Return topological sort result.
     SortResult tsort()
     {
         auto ctx = SortContext(this);
         for (auto i = 0; i < nodesCount; ++i)
         {
-            writefln("top iter %d", i);
+            debugPrintfln("top iter %d", i);
             const foundCycle = visit(ctx, i);
 
             if (foundCycle.selector is SortContext.VisitStatus.Selector.cycleFound)
             {
-                writeln("cycle!");
+                debugPrintln("cycle!");
                 return SortResult.makeCycle(foundCycle.cycle);
             }
         }
-        writeln("sorted!");
+        debugPrintln("sorted!");
         return SortResult.makeSorted(ctx.sorted);
+    }
+
+    private static void printEdges(const Edges edges)
+    {
+        auto f = (const Edge n) => writefln("edge %d -> %d", n.fromCell, n.toCell);
+        each!(f)(edges);
     }
 }
 
-void printEdges(const Edges edges)
-{
-    auto f = (const Edge n) => writefln("edge %d -> %d", n.fromCell, n.toCell);
-    each!(f)(edges);
-}
 
+/// Topological sort result.
 struct SortResult
 {
+    /// One of the outcomes.
     enum Selector
     {
         sorted,
         cycle,
     }
 
+    /// Data selector.
     Selector result;
+
     union
     {
+        /// Sorted data if result is sorted.
         CellIndex[] sorted;
+        /// First spotted node to be in a cycle.
         CellIndex cycle;
     }
 
-    this(CellIndex cycle)
+    private this(CellIndex cycle)
     {
         result = Selector.cycle;
         this.cycle = cycle;
     }
 
-    this(CellIndex[] sorted)
+    private this(CellIndex[] sorted)
     {
         this.sorted = sorted;
         result = Selector.sorted;
     }
 
-    static SortResult makeCycle(CellIndex c)
+    private static SortResult makeCycle(CellIndex c)
     {
         return SortResult(c);
     }
 
-    static SortResult makeSorted(CellIndex[] s)
+    private static SortResult makeSorted(CellIndex[] s)
     {
         return SortResult(s);
     }
@@ -195,7 +221,7 @@ private struct SortContext
             auto mapped = map!fvisit(nodesOfN);
             writefln("%d mapped", n);
             // here there's probably compiler bug, since it fails
-            auto foundRange = find!is_cycle(mapped);
+            const foundRange = find!is_cycle(mapped);
             //writefln("range: %s", foundRange);
             markPerm(n);
             addToSorted(n);
@@ -213,22 +239,22 @@ private struct SortContext
     }
 }
 
-SortContext.VisitStatus visit(SortContext ctx, CellIndex n)
+private SortContext.VisitStatus visit(SortContext ctx, CellIndex n)
 {
-    writefln("%d visit", n);
+    debugPrintfln("%d visit", n);
     if (find(ctx.permMarked, n).empty == false)
     {
-        writeln("already perm");
+        debugPrintln("already perm");
         return SortContext.VisitStatus();
     }
     else if (find(ctx.tempMarked, n).empty == false)
     {
-        writefln("%d early cycle", n);
+        debugPrintfln("%d early cycle", n);
         return SortContext.VisitStatus(n);
     }
     ctx.markTemp(n);
     auto nodesOfN = ctx.graph.allDestFrom(n);
-    writefln("%d nodesOfN = %s", n, nodesOfN);
+    debugPrintfln("%d nodesOfN = %s", n, nodesOfN);
 
     auto foundCycle = SortContext.VisitStatus();
     foreach (i; nodesOfN)
@@ -241,31 +267,31 @@ SortContext.VisitStatus visit(SortContext ctx, CellIndex n)
     ctx.addToSorted(n);
     if (!foundCycle.isCycle)
     {
-        writefln("%d ctx continue", n);
+        debugPrintfln("%d ctx continue", n);
         return SortContext.VisitStatus();
     }
     assert(foundCycle.selector is SortContext.VisitStatus.Selector.cycleFound);
-    writefln("%d ctx return cycle %s", n, foundCycle);
+    debugPrintfln("%d ctx return cycle %s", n, foundCycle);
     return foundCycle;
 }
 
 unittest
 {
-    auto fvisit(CellIndex c)
+    auto fvisit(in CellIndex c)
     {
         if (c & 1)
             return SortContext.VisitStatus();
         return SortContext.VisitStatus(c);
     }
 
-    auto isCycle(SortContext.VisitStatus s)
+    auto isCycle(in SortContext.VisitStatus s)
     {
         return s.selector is SortContext.VisitStatus.Selector.cycleFound;
     }
 
     auto mapped = map!fvisit([2]);
     auto found = find!isCycle(mapped);
-    assert(!found.empty);
+    assert(!found.empty());
     assert(found.front.selector is SortContext.VisitStatus.Selector.cycleFound);
 }
 
