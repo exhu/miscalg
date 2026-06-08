7a68849da85702d070dd7ec9aec1abbab93babcb
Yury Benesh <yurybdev@gmail.com>
2026-06-08 09:31:59 +0300

    editorconfig

--- sdlffc/sdlffclib.c
+++ sdlffc/sdlffclib.c
@@ -1,13 +1,13 @@
 #include "sdlffclib.h"
 #include "SDL3/SDL_scancode.h"
 #include "sdlffclib_private.h"
+#include <SDL3/SDL_dialog.h>
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_hints.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
-#include <SDL3/SDL_dialog.h>
 
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
@@ -86,7 +86,8 @@ static void dialog_cb(void *userdata, const char * const *filelist, int filter) 
 #endif
 
 void sdlffclib_main_loop(SdlffContext *context) {
-  //SDL_ShowOpenFileDialog(dialog_cb, NULL, context->window, NULL, 0, NULL, false);
+  // SDL_ShowOpenFileDialog(dialog_cb, NULL, context->window, NULL, 0, NULL,
+  // false);
   SDL_Event event;
   bool should_break = false;
   while (!should_break && SDL_WaitEvent(&event)) {
@@ -99,8 +100,8 @@ void sdlffclib_main_loop(SdlffContext *context) {
       break;
     case SDL_EVENT_KEY_DOWN:
       if (event.key.scancode == SDL_SCANCODE_Q) {
-	should_break = true;
-	break;
+        should_break = true;
+        break;
       }
 
     default:;
@@ -110,35 +111,42 @@ void sdlffclib_main_loop(SdlffContext *context) {
 }
 
 bool sdlffclib_fileinfo(const char *file_path) {
-    AVFormatContext *ic = NULL;
-    int result = avformat_open_input(&ic, file_path, NULL, NULL);
-    if (result < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't open %s: %d", file_path, result);
-	return false;
-    }
-    result = avformat_find_stream_info(ic, NULL);
-    if (result < 0) {
-	avformat_close_input(&ic);
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't read stream info %s: %d", file_path, result);
-	return false;
-    }
-    SDL_Log("Long name: %s", ic->iformat->long_name);
-    SDL_Log("Name: %s", ic->iformat->name);
-    SDL_Log("Mime: %s", ic->iformat->mime_type);
-    SDL_Log("Extensions: %s", ic->iformat->extensions);
-    SDL_Log("Streams: %d", ic->nb_streams);
-    for (unsigned i = 0; i < ic->nb_streams; ++i) {
-      SDL_Log("Stream: %d", i);
-      AVStream *stream = ic->streams[i];
-      SDL_Log("Time base: %d/%d", stream->time_base.num, stream->time_base.den);
-      SDL_Log("Duration: %ld", stream->duration);
-      double duration = (double)(stream->duration * stream->time_base.num) / stream->time_base.den;
-      SDL_Log("Duration on time base: %f", duration);
-      SDL_Log("Frame rate: %d/%d", stream->r_frame_rate.num, stream->r_frame_rate.den);
-      const enum AVMediaType media_type = stream->codecpar->codec_type;
-      SDL_Log("Codec type: %d, %s", media_type, media_type == AVMEDIA_TYPE_AUDIO ? "audio" : media_type == AVMEDIA_TYPE_VIDEO ? "video" : "other" );
-    }
-
+  AVFormatContext *ic = NULL;
+  int result = avformat_open_input(&ic, file_path, NULL, NULL);
+  if (result < 0) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't open %s: %d",
+                 file_path, result);
+    return false;
+  }
+  result = avformat_find_stream_info(ic, NULL);
+  if (result < 0) {
     avformat_close_input(&ic);
-    return true;
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                 "Couldn't read stream info %s: %d", file_path, result);
+    return false;
+  }
+  SDL_Log("Long name: %s", ic->iformat->long_name);
+  SDL_Log("Name: %s", ic->iformat->name);
+  SDL_Log("Mime: %s", ic->iformat->mime_type);
+  SDL_Log("Extensions: %s", ic->iformat->extensions);
+  SDL_Log("Streams: %d", ic->nb_streams);
+  for (unsigned i = 0; i < ic->nb_streams; ++i) {
+    SDL_Log("Stream: %d", i);
+    AVStream *stream = ic->streams[i];
+    SDL_Log("Time base: %d/%d", stream->time_base.num, stream->time_base.den);
+    SDL_Log("Duration: %ld", stream->duration);
+    double duration = (double)(stream->duration * stream->time_base.num) /
+                      stream->time_base.den;
+    SDL_Log("Duration on time base: %f", duration);
+    SDL_Log("Frame rate: %d/%d", stream->r_frame_rate.num,
+            stream->r_frame_rate.den);
+    const enum AVMediaType media_type = stream->codecpar->codec_type;
+    SDL_Log("Codec type: %d, %s", media_type,
+            media_type == AVMEDIA_TYPE_AUDIO   ? "audio"
+            : media_type == AVMEDIA_TYPE_VIDEO ? "video"
+                                               : "other");
+  }
+
+  avformat_close_input(&ic);
+  return true;
 }
